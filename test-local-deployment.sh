99356b8384114537301a0ec2db21e6246b6fa80c
HalitM <halitmizrk@gmail.com>
2024-01-22 16:16:59 +0000

    added docker-compose file and script for local deployment

--- test-local-deployment.sh
+++ test-local-deployment.sh
@@ -1,1 +1,5 @@
+#####################
+## bu script docker server icin uygulamayi mysql db kullanarak docker ile ayaga kaldirir
+##################### 
+
 docker-compose -f docker-compose-local.yml up
